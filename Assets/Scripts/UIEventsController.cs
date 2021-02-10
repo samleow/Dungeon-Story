b286e304b872a4795947814795199c94a524a802
Samuel Leow <samuel.leow@gmail.com>
2021-02-10 23:32:37 +0800

    Added scene transitions to current scenes.

--- Assets/Scripts/UIEventsController.cs
+++ Assets/Scripts/UIEventsController.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIEventsController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Load Scene
+    public void LoadScene(string newScene)
     {
-        
+        SceneManager.LoadScene(newScene);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Exit application
+    public void ExitApplication()
     {
-        
+        Application.Quit();
     }
+
 }
