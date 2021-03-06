8bd0206ce7c45a7ca4eb0081b5872406dd3f48c7
valerianlow <valerianlow@gmail.com>
2021-03-06 18:15:43 +0800

    Smoother/faster leaderboard load

--- Assets/Scripts/makeLeaderboard.cs
+++ Assets/Scripts/makeLeaderboard.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
 
 public class makeLeaderboard : MonoBehaviour
 {
@@ -11,114 +13,113 @@ public class makeLeaderboard : MonoBehaviour
     private Text text;
 
     void Start()
-    {
-        //SceneManager.LoadScene("LeaderboardPage");    
-    
-        StartCoroutine(loadLeaderboard());
+    { 
+        int x = -220;   //position for text label
+        int y = 170;    //position for text label
 
-    }
-    IEnumerator loadLeaderboard()
-    {
-        UnityWebRequest www = UnityWebRequest.Get("http://valerianlow123.000webhostapp.com/getLeaderboard.php");
-        //WWW getScores = new WWW("http://valerianlow123.000webhostapp.com/getLeaderboard.php?");
-        yield return www.SendWebRequest();
-        if (www.isNetworkError || www.isHttpError)
+        int rank = 1;   //user rank
+
+        Font arial;
+        arial = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+
+        // Create canvas
+        GameObject canvasGO = new GameObject();
+        canvasGO.name = "Canvas";
+        canvasGO.AddComponent<Canvas>();
+        canvasGO.AddComponent<CanvasScaler>();
+        canvasGO.AddComponent<GraphicRaycaster>();
+
+        // Get canvas from the GameObject.
+        Canvas canvas;
+        canvas = canvasGO.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        // Create text 
+        GameObject textGO = new GameObject();
+        textGO.transform.parent = canvasGO.transform;
+        textGO.AddComponent<Text>();
+
+        // Edit text attribute
+        text = textGO.GetComponent<Text>();
+        text.text = "LEADERBOARD";
+        text.fontSize = 50;
+        text.font = arial;
+        text.alignment = TextAnchor.MiddleCenter;
+
+        // Positioning and size of text
+        RectTransform rectTransform;
+        rectTransform = text.GetComponent<RectTransform>();
+        rectTransform.localPosition = new Vector3(0, 250, 0);
+        rectTransform.sizeDelta = new Vector2(600, 60);
+
+        // Read from preloaded leaderboard
+        string path = "Assets/Resources/leaderboard.txt";
+        string line;
+        
+        //Read the text from directly from the leaderboard.txt file
+        StreamReader reader = new StreamReader(path);
+        line = reader.ReadToEnd();
+        string[] nameScores = line.Split('\n');
+        reader.Close();
+
+        //print leaderboard
+        for (int i = 0; i < nameScores.Length - 1; i = i + 2)
         {
-            //Debug.Log(www.error);
-        }
-        else
-        {            
-            string [] nameScores = www.downloadHandler.text.Split(' ');
-            Debug.Log(nameScores[8]);
-            //yield return nameScores;
-            //SceneManager.LoadScene("LeaderboardPage");    
-            int x = -220;
-            int y = 170;
-
-            int rank = 1;
-
-            Font arial;
-            arial = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
-
-            GameObject canvasGO = new GameObject();
-            canvasGO.name = "Canvas";
-            canvasGO.AddComponent<Canvas>();
-            canvasGO.AddComponent<CanvasScaler>();
-            canvasGO.AddComponent<GraphicRaycaster>();
-
-            // Get canvas from the GameObject.
-            Canvas canvas;
-            canvas = canvasGO.GetComponent<Canvas>();
-            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-
-            GameObject textGO = new GameObject();
+            //disp Rank
+            textGO = new GameObject();
             textGO.transform.parent = canvasGO.transform;
             textGO.AddComponent<Text>();
-
             text = textGO.GetComponent<Text>();
-            text.text = "LEADERBOARD";
-            text.fontSize = 50;
+            if (i != 0 && nameScores[i + 1] == nameScores[i - 1])
+            {
+                text.text = "=";
+            }
+            else
+            {
+                text.text = rank.ToString() + ".";
+            }
+
+            text.fontSize = 30;
             text.font = arial;
             text.alignment = TextAnchor.MiddleCenter;
 
-            RectTransform rectTransform;
             rectTransform = text.GetComponent<RectTransform>();
-            rectTransform.localPosition = new Vector3(0, 250, 0);
-            rectTransform.sizeDelta = new Vector2(600, 60);
-            //Console.WriteLine(nameScores[0]);
-            for (int i = 0; i < nameScores.Length-1; i = i + 2)
-            {
-                //disp Rank
-                if(i!=0 && nameScores[i+1] != nameScores[i-1])
-                {
-                    rank++;
-                }
-                textGO = new GameObject();
-                textGO.transform.parent = canvasGO.transform;
-                textGO.AddComponent<Text>();
+            rectTransform.localPosition = new Vector3(x, y, 0);
+            rectTransform.sizeDelta = new Vector2(50, 60);
 
-                text = textGO.GetComponent<Text>();
-                text.text = rank.ToString() + ".";
-                text.fontSize = 30;
-                text.font = arial;
-                text.alignment = TextAnchor.MiddleCenter;
+            //disp Name
+            textGO = new GameObject();
+            textGO.transform.parent = canvasGO.transform;
+            textGO.AddComponent<Text>();
+            text = textGO.GetComponent<Text>();
+            text.text = nameScores[i];
+            text.fontSize = 30;
+            text.font = arial;
+            text.alignment = TextAnchor.MiddleLeft;
 
-                rectTransform = text.GetComponent<RectTransform>();
-                rectTransform.localPosition = new Vector3(x, y, 0);
-                rectTransform.sizeDelta = new Vector2(50, 60);
+            rectTransform = text.GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(x + 200, y, 0);
+            rectTransform.sizeDelta = new Vector2(300, 60);
 
-                //disp Name
-                textGO = new GameObject();
-                textGO.transform.parent = canvasGO.transform;
-                textGO.AddComponent<Text>();
+            //disp Score
+            textGO = new GameObject();
+            textGO.transform.parent = canvasGO.transform;
+            textGO.AddComponent<Text>();
+            text = textGO.GetComponent<Text>();
+            text.text = nameScores[i + 1];
+            text.fontSize = 30;
+            text.font = arial;
+            text.alignment = TextAnchor.MiddleCenter;
 
-                text = textGO.GetComponent<Text>();
-                text.text = nameScores[i];
-                text.fontSize = 30;
-                text.font = arial;
-                text.alignment = TextAnchor.MiddleCenter;
-
-                rectTransform = text.GetComponent<RectTransform>();
-                rectTransform.localPosition = new Vector3(x+100, y, 0);
-                rectTransform.sizeDelta = new Vector2(300, 60);
-                //disp Score
-                textGO = new GameObject();
-                textGO.transform.parent = canvasGO.transform;
-                textGO.AddComponent<Text>();
-
-                text = textGO.GetComponent<Text>();
-                text.text = nameScores[i+1];
-                text.fontSize = 30;
-                text.font = arial;
-                text.alignment = TextAnchor.MiddleCenter;
-
-                rectTransform = text.GetComponent<RectTransform>();
-                rectTransform.localPosition = new Vector3(x + 400, y, 0);
-                rectTransform.sizeDelta = new Vector2(300, 60);
-                y = y - 60;
-            }
+            rectTransform = text.GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(x + 400, y, 0);
+            rectTransform.sizeDelta = new Vector2(300, 60);
+            y = y - 60; // move y position down
+            rank++; //increase rank
         }
+
     }
+    
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Scripts/preloadLeaderboard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEditor;
+using System.IO;
+
+public class preloadLeaderboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(loadLeaderboard());
+    }
+
+    IEnumerator loadLeaderboard()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://valerianlow123.000webhostapp.com/getLeaderboard.php");
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            string[] nameScores = www.downloadHandler.text.Split(' ');
+            string path = "Assets/Resources/leaderboard.txt";
+
+            //Write some text to the test.txt file
+            StreamWriter writer = new StreamWriter(path, false);
+            for (int i = 0; i < nameScores.Length - 1; i++){
+                writer.WriteLine(nameScores[i]);
+            }
+            writer.Close();
+        }
+    }
+            // Update is called once per frame
+     void Update()
+    {
+        
+    }
+}
