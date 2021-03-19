582684ae0636b9e7fdd84e2747b355d89a55744e
ChinHaoX3 <40873148+ChinHaoX3@users.noreply.github.com>
2021-03-19 17:11:09 +0800

    Update PlayModePage UI

---
+++ Assets/Scripts/PlayModePopUpScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayModePopUpScript : MonoBehaviour
+{
+    // Declare gameobject variable
+    public GameObject dungeonPopup;
+    public GameObject challengePopup;
+    
+    // open Dungeon popup Window
+    public void openDungeonPopup()
+    {
+        dungeonPopup.SetActive(true);
+    }
+
+    // open challenge popup window
+    public void openChallengePopup()
+    {
+        challengePopup.SetActive(true);
+    }
+    
+    // close dungeon popup window 
+    public void closeDungeonPopup()
+    {
+        dungeonPopup.SetActive(false);
+    }
+
+    // close challenge popup window
+    public void closeChallengePopup()
+    {
+        challengePopup.SetActive(false);
+    }
+   
+}
