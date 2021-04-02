34559557c3ed5b55405700c80f53da4e2ed7212d
duckygoh <53110445+duckygoh@users.noreply.github.com>
2021-04-02 23:11:10 +0800

    Create Test_AddQuestion.cs

---
+++ Assets/Test/Editor/Test_AddQuestion.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Altom.AltUnityDriver;
+
+public class Test_AddQuestion
+{
+    public AltUnityDriver AltUnityDriver;
+    //Before any test it connects with the socket
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        AltUnityDriver = new AltUnityDriver();
+    }
+
+    //At the end of the test closes the connection with the socket
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        AltUnityDriver.Stop();
+    }
+
+    [Test]
+    public void TestAddQuestion()
+    {
+        //Here you can write the test
+        // Load starting scene
+        AltUnityDriver.LoadScene("AddQuesPage");
+
+        // In LoginPage Try login
+        AltUnityDriver.FindObject(By.NAME, "Question").SetText("testing question");
+        AltUnityDriver.FindObject(By.NAME, "Option1").SetText("option 1");
+        AltUnityDriver.FindObject(By.NAME, "Option2").SetText("option 2");
+        AltUnityDriver.FindObject(By.NAME, "Option3").SetText("option 3");
+        AltUnityDriver.FindObject(By.NAME, "Answer").SetText("a");
+        AltUnityDriver.FindObject(By.NAME, "Difficulty").SetText("1");
+        AltUnityDriver.FindObject(By.NAME, "Save").ClickEvent();
+        AltUnityDriver.WaitForCurrentSceneToBe("AddQuesPage");
+
+    }
+
+}
