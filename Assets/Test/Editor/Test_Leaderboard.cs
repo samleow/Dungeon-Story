643bd07ebee47b4b2e9c777019f3b79ea4f8fb8b
TokJingXian <tokjingxian@gmail.com>
2021-04-02 23:16:47 +0800

    stuffs

---
+++ Assets/Test/Editor/Test_Leaderboard.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Altom.AltUnityDriver;
+
+public class Test_Leaderboard
+{
+    public AltUnityDriver AltUnityDriver;
+    //Before any test it connects with the socket
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        AltUnityDriver = new AltUnityDriver();
+    }
+
+    //At the end of the test closes the connection with the socket
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        AltUnityDriver.Stop();
+    }
+
+    [Test]
+    public void TestLeaderBoard()
+    {
+        AltUnityDriver.LoadScene("TitlePage");
+        AltUnityDriver.FindObject(By.NAME, "Leaderboard Button").ClickEvent();
+        AltUnityDriver.WaitForCurrentSceneToBe("LeaderboardPage");
+    }
+}
---
+++ Assets/Test/Editor/Test_MuteVol.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Altom.AltUnityDriver;
+
+public class Test_MuteVol
+{
+    public AltUnityDriver AltUnityDriver;
+    //Before any test it connects with the socket
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        AltUnityDriver = new AltUnityDriver();
+    }
+
+    //At the end of the test closes the connection with the socket
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        AltUnityDriver.Stop();
+    }
+
+    [Test]
+    public void TestMuteVol()
+    {
+        AltUnityDriver.LoadScene("TitlePage");
+        AltUnityDriver.FindObject(By.NAME, "Toggle").ClickEvent();
+    }
+}
