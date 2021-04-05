ae49651be443267f99e308b6a8c7e8b2f0575891
ChinHaoX3 <40873148+ChinHaoX3@users.noreply.github.com>
2021-04-05 23:59:50 +0800

    Update PerformanceTest.cs

--- Assets/Tests/PerformanceTest.cs
+++ Assets/Tests/PerformanceTest.cs
@@ -1,94 +1,173 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using NUnit.Framework;
 using Unity.PerformanceTesting;
 using UnityEngine;
-using UnityEngine.Profiling;
+using UnityEngine.TestTools;
+using Unity.PerformanceTesting;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
-// define custom variable for test test
-[TestFixture(1)]
-public class PerformanceTest
+using UnityEngine.Profiling;
+using System;
+using System.Runtime.InteropServices;
+using TMPro;
+namespace Tests
 {
-    private int _value;
-
-    public PerformanceTest(int value)
+    public class PerformanceTest
     {
-        _value = value;
-    }
-
-    [PerformanceUnityTest]
-    public IEnumerator SampleTest()
-    {
-        // It goes to the group name "Time"
-        // Measure the execution time of a method (measure 10 times, usage an average of 5 measurements)
-        Measure.Method(() =>
+        // Testing for Login
+        [UnityTest, Performance]
+        public IEnumerator Login()
         {
-            for (int i = 0; i < 1000000; i++)
+            // stopwatch to measure execution time 
+            stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Reset();
+            stopwatch.Start();
+            
+            using (Measure.Scope(new SampleGroupDefinition("Setup.LoadScene")))
             {
+                SceneManager.LoadScene("Login");
             }
-        })
-            .MeasurementCount(10)
-            .IterationsPerMeasurement(5)
-            .Run();
-
-
-        // It goes to the group name "Time"
-        // Measure the execution time of 10 frames, wait for 5 frames to warm up
-        yield return Measure.Frames()
-            .WarmupCount(5)
-            //.ProfilerMarkers(groupDefinitions) // if we just want to measure profiler without record frames
-            //.DontRecordFrametime()
-            .MeasurementCount(11)
-            .Run();
-
-        // Measure the execution time of all frames
-        using (Measure.Frames().Scope(new SampleGroupDefinition("Group 1")))
-        {
-            // your code
             yield return null;
-            yield return null;
+
+
+            yield return new WaitForSeconds(.001f);
+
+            TMP_InputField Email = GameObject.Find("login_username").GetComponent<TMP_InputField>();
+            Email.text = "a";
+
+
+            TMP_InputField Password = GameObject.Find("login_password").GetComponent<TMP_InputField>();
+
+            Password.text = "a";
+
+            yield return new WaitForSeconds(.001f);
+
+            UnityEngine.UI.Button LoginButton = GameObject.Find("Login Button").GetComponent<UnityEngine.UI.Button>();
+            LoginButton.onClick.Invoke();
+
+            yield return Measure.Frames().Run();
+            stopwatch.Stop();
+            Debug.Log(stopwatch.ElapsedMilliseconds);
         }
-
-        // measure profiler data
-        SampleGroupDefinition[] groupDefinitions =
+        // measure memory << server side performance measurement 
+        [UnityTest, Performance]
+        public void Measure_Memory()
         {
-            new SampleGroupDefinition("PlayerLoop"),
-            new SampleGroupDefinition("Camera.Render"),
-        };
-        using (Measure.ProfilerMarkers(groupDefinitions))
-        {
+            var allocated = new SampleGroupDefinition("TotalAllocatedMemory", SampleUnit.Megabyte);
+            var reserved = new SampleGroupDefinition("TotalReservedMemory", SampleUnit.Megabyte);
             for (var i = 0; i < 60; i++)
             {
+                Measure.Custom(allocated, Profiler.GetTotalAllocatedMemoryLong() / 1048576f);
+                Measure.Custom(reserved, Profiler.GetTotalReservedMemoryLong() / 1048576f);
                 yield return null;
             }
         }
-
-        // measure memory
-        var allocated = new SampleGroupDefinition("TotalAllocatedMemory", SampleUnit.Megabyte);
-        var reserved = new SampleGroupDefinition("TotalReservedMemory", SampleUnit.Megabyte);
-        for (var i = 0; i < 60; i++)
+        // Scene Measurement
+        [UnityTest, Performance]
+        public IEnumerator Rendering_Scene()
         {
-            Measure.Custom(allocated, Profiler.GetTotalAllocatedMemoryLong() / 1048576f);
-            Measure.Custom(reserved, Profiler.GetTotalReservedMemoryLong() / 1048576f);
-            yield return null;
-        }
+            stopwatch.Reset();
+            stopwatch.Start();
 
-        // load scene
-        using (Measure.Scope(new SampleGroupDefinition("Group 3 LandingPage")))
-        {
-            SceneManager.LoadScene("Assets/Scenes/LandingPage.unity");
-        }
-
-        // measure instantiate of objects
-        using (Measure.Scope(new SampleGroupDefinition("Group 4")))
-        {
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            for (var i = 0; i < 5000; i++)
+            using (Measure.Scope(new SampleGroupDefinition("Test LandingPage")))
             {
-                UnityEngine.Object.Instantiate(cube);
+                SceneManager.LoadScene("LandingPage");
             }
-        }
+            yield return null;
 
-        yield return null;
+            yield return Measure.Frames().Run();
+
+            //yield return Measure.Frames().Run();
+            stopwatch.Stop();
+            Debug.Log(stopwatch.ElapsedMilliseconds);
+        }
+        //Leaderboard test
+        [UnityTest, Performance]
+        public IEnumerator Gameflow_Test()
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            using (Measure.Scope(new SampleGroupDefinition("Setup.LoadScene")))
+            {
+                SceneManager.LoadScene("Login");
+            }
+            yield return null;
+
+
+            yield return new WaitForSeconds(.001f);
+
+            TMP_InputField Email = GameObject.Find("login_username").GetComponent<TMP_InputField>();
+            Email.text = "a";
+
+
+            TMP_InputField Password = GameObject.Find("login_password").GetComponent<TMP_InputField>();
+
+            Password.text = "a";
+
+            yield return new WaitForSeconds(.001f);
+
+            UnityEngine.UI.Button LoginButton = GameObject.Find("Login Button").GetComponent<UnityEngine.UI.Button>();
+            LoginButton.onClick.Invoke();
+
+            yield return new WaitForSeconds(1);
+
+            UnityEngine.UI.Button leaderBoardButton = GameObject.Find("Leaderboard Button").GetComponent<UnityEngine.UI.Button>();
+            leaderBoardButton.onClick.Invoke();
+
+            yield return new WaitForSeconds(2);
+
+            UnityEngine.UI.Button LoginButton = GameObject.Find("Button").GetComponent<UnityEngine.UI.Button>(); // back button 
+            LoginButton.onClick.Invoke();
+
+            yield return new WaitForSeconds(1);
+
+            UnityEngine.UI.Button StartButton = GameObject.Find("Start Button").GetComponent<UnityEngine.UI.Button>(); // start button 
+            StartButton.onClick.Invoke();
+
+            yield return new WaitForSeconds(2);
+
+            //DungeonMode Button
+            UnityEngine.UI.Button DungeonModeButton = GameObject.Find("DungeonMode Button").GetComponent<UnityEngine.UI.Button>(); 
+            DungeonModeButton.onClick.Invoke();
+
+            yield return new WaitForSeconds(2);
+
+            //World Button
+            UnityEngine.UI.Button World1 = GameObject.Find("World1").GetComponent<UnityEngine.UI.Button>(); 
+            World1.onClick.Invoke();
+
+            yield return new WaitForSeconds(2);
+
+            //Section 1 Button
+            UnityEngine.UI.Button Section1 = GameObject.Find("Section 1 Button").GetComponent<UnityEngine.UI.Button>(); 
+            Section1.onClick.Invoke();
+
+            yield return new WaitForSeconds(1);
+
+            // Character - Warrior
+            //Warrior Button
+            UnityEngine.UI.Button Warrior = GameObject.Find("Warrior").GetComponent<UnityEngine.UI.Button>(); 
+            Warrior.onClick.Invoke();
+
+            //Mage Button
+            UnityEngine.UI.Button Mage = GameObject.Find("Mage").GetComponent<UnityEngine.UI.Button>();  
+            Mage.onClick.Invoke();
+
+            //Barbarian Button
+            UnityEngine.UI.Button Barbarian = GameObject.Find("Barbarian").GetComponent<UnityEngine.UI.Button>();  
+            Barbarian.onClick.Invoke();
+
+            //Confirm Button
+            UnityEngine.UI.Button ConfirmButton = GameObject.Find("Confirm Button").GetComponent<UnityEngine.UI.Button>(); 
+            ConfirmButton.onClick.Invoke();
+
+            yield return Measure.Frames().Run();
+
+            //yield return Measure.Frames().Run();
+            stopwatch.Stop();
+            Debug.Log(stopwatch.ElapsedMilliseconds);
+        }
     }
 }
